d1dede286b8782086f37b93931e947615520dd5b
FuchsCoder <69680700+FuchsCrafter@users.noreply.github.com>
2024-11-16 00:09:50 +0100

    add comments

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ frame = 0
 
 
 class Tube():
-    def __init__(self, y, gap_height=200): 
+    def __init__(self, y, gap_height=200): # init tubes
         self.color = (50,200,0)
 
         wid = 50
@@ -32,16 +32,16 @@ class Tube():
 
         self.x = WIDTH-wid
     
-    def draw(self):
+    def draw(self): # handles drawing
         pygame.draw.rect(screen, self.color, self.rec_top)
         pygame.draw.rect(screen, self.color, self.rec_dwn)
 
-    def set_x(self, x):
+    def set_x(self, x): # handles setting x
         self.x = x
         self.rec_top.x = x
         self.rec_dwn.x = x
     
-    def decrease_x(self, x=1):
+    def decrease_x(self, x=1): # handles decreasing x for moving tubes
         self.x -= x
         self.rec_top.x -= x
         self.rec_dwn.x -= x
@@ -51,8 +51,6 @@ tubez = Tube(0)
 curr_y = 0
 currynormalized = (HEIGHT/2) + curr_y
 
-
-
 player = 100
 jumping = 0
 
@@ -60,32 +58,41 @@ jumpFactor = 0.14
 score = 0
 
 while running:
+
+    # handle quitting
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
 
+    # handle click
     if pygame.mouse.get_pressed(num_buttons=3)[0]:
         jumping = -jumpFactor*30
         player += jumping
         jumping += jumpFactor
 
+    # handle player on the ground
     if player < HEIGHT-50:
         player += jumping
         jumping += jumpFactor
 
+    # clear display
     screen.fill((45, 255, 227))
 
+    # handle tubes
     tubez.draw()
     tubez.decrease_x(5)
 
+    # draw player
     pygame.draw.rect(screen,(255,0,0),pygame.Rect(100,player,50,50))
 
+    # handle score display
     text_surface = deffont.render(f'Score: {score}', False, (0, 0, 0))
     screen.blit(text_surface, (0,0))
 
-
+    # flip display
     pygame.display.flip()
 
+    # handle Game over
     if tubez.x <=150 and tubez.x >=100:
         if player>currynormalized+100 or player<currynormalized-100:
             print("GAME OVER")
@@ -99,14 +106,12 @@ while running:
             screen.blit(game_over2, (WIDTH/2, HEIGHT/2+15))
             screen.blit(game_over3, (WIDTH/2, HEIGHT/2+45))
 
-
-
             pygame.display.flip()
 
             time.sleep(10)
             running = False
 
-
+    # handle tubes reaching left screen border
     if (tubez.x <= 0):
         del tubez
         score += 1
