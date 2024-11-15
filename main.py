6f978e5b68f642067777cb63c6bbf5e2c295b15d
FuchsCoder <69680700+FuchsCrafter@users.noreply.github.com>
2024-11-15 22:54:19 +0100

    barebones and rectangles

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 # Example file showing a basic pygame "game loop"
 import pygame
-
+from math import sin
 # pygame setup
 pygame.init()
 screen = pygame.display.set_mode((1280, 720))
 clock = pygame.time.Clock()
 running = True
+frame = 0
+
 
 while running:
     # poll for events
@@ -15,13 +17,20 @@ while running:
             running = False
 
     # fill the screen with a color to wipe away anything from last frame
-    screen.fill("purple")
+    screen.fill((45, 255, 227))
 
-    # RENDER YOUR GAME HERE
+
+
+    pygame.draw.rect(screen,(255,0,0,),pygame.Rect(sin(frame/70)*100,0,100,100))
+    pygame.draw.rect(screen,(255,0,0,),pygame.Rect(sin(frame/70)*100,0,100,100))
+
+
+
 
     # flip() the display to put your work on screen
     pygame.display.flip()
 
+    frame += 1
     clock.tick(60)  # limits FPS to 60
 
 pygame.quit()
